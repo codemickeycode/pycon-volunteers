2cf0ad5f0d4a3ed389935f8ace99f748e1ae0bd7
Micaela Reyes <codemickeycode@gmail.com>
2015-03-14 04:56:22 +0800

    Fix display of related committees in admin

--- volunteer/admin.py
+++ volunteer/admin.py
@@ -6,10 +6,18 @@ class CommiteeAdmin(admin.ModelAdmin):
     list_display = ('committee_id', 'name', 'slots_available', 'slots_taken')
 
 class VolunteerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'nickname', 'email', 'contactno', 'message', 'created_date', 'committee_id')
+    list_display = ('name', 'nickname', 'email', 'contactno',
+        'message', 'created_date',
+        'committee_id', 'related_committee')
+    def related_committee(self, obj):
+        return '%s'%(obj.committee_id.name)
+    related_committee.short_description = 'Committee Name'
 
 class CommitteeChairAdmin(admin.ModelAdmin):
-    list_display = ('name', 'committee_id')
+    list_display = ('name', 'committee_id', 'related_committee')
+    def related_committee(self, obj):
+        return '%s'%(obj.committee_id.name)
+    related_committee.short_description = 'Committee Name'
 
 admin.site.register(Committee, CommiteeAdmin)
 admin.site.register(Volunteer, VolunteerAdmin)
